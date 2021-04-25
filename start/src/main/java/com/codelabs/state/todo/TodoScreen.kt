b7c1138a19a25bcfc08e693b65038cc7e01b87b9
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-04-25 15:52:43 -0700

    * inlined TodoInputText into TodoItemInput * adds a submit lambda to handle submit of TodoItem, used in both onImeAction and onClick of TodoEditButton

--- start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -113,37 +113,39 @@ private fun randomTint(): Float {
     return Random.nextFloat().coerceIn(0.3f, 0.9f)
 }
 
-@Composable
-fun ToDoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) {
-    TodoInputText(text = text, onTextChange, modifier) {
-
-    }
-}
+//@Composable
+//fun ToDoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) {
+//    TodoInputText(text = text, onTextChange, modifier) {
+//
+//    }
+//}
 
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
     val (icon, setIcon) = remember {mutableStateOf(TodoIcon.Default)}
     val iconsVisible = text.isNotBlank()
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setIcon(TodoIcon.Default)
+        setText("")
+    }
     Column {
         Row(
             Modifier
                 .padding(horizontal = 16.dp)
                 .padding(top = 16.dp)
         ) {
-            ToDoInputTextField(
+            TodoInputText(
                 text = text,
                 onTextChange = setText,
                 modifier = Modifier
                     .weight(1f)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp),
+                onImeAction = submit
             )
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text, icon))
-                    setIcon(TodoIcon.Default)
-                    setText("")
-                },
+                onClick = submit,
                 text = "Add",
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank()
